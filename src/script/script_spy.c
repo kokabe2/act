3739aecf29fd97325fa6a7af83d8584a403d205f
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-20 11:56:54 +0900

    Add Script spy

---
+++ src/script/script_spy.c
@@ -0,0 +1,53 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "script/script_spy.h"
+
+#include "bleu/v1/heap.h"
+#include "script/script_base_protected.h"
+
+typedef struct {
+  ScriptBaseStruct base;
+  bool deleted;
+  bool done;
+} ScriptSpyStruct, *ScriptSpy;
+
+static void DeleteSpy(Command* self) { ((ScriptSpy)*self)->deleted = true; }
+
+static void DoSpy(Command base) {
+  ScriptSpy self = (ScriptSpy)base;
+  ActiveObjectEngine e = self->base.engine;
+  self->done = true;
+  e->AddCommand(e, self->base.notification_command);
+}
+
+static Command New(const char* name) {
+  ScriptSpy self = (ScriptSpy)heap->New(sizeof(ScriptSpyStruct));
+  self->base.impl.Delete = DeleteSpy;
+  self->base.impl.Do = DoSpy;
+  self->base.name = name;
+  return (Command)self;
+}
+
+static void Delete(Command* self) { heap->Delete((void**)self); }
+
+static bool DeleteCalled(Command self) { return ((ScriptSpy)self)->deleted; }
+
+static bool RunCalled(Command self) { return ((ScriptSpy)self)->done; }
+
+static ActiveObjectEngine GetEngine(Command self) { return ((ScriptBase)self)->engine; }
+
+static Command GetNotificationCommand(Command self) { return ((ScriptBase)self)->notification_command; }
+
+static void SetErrorCode(Command self, int error_code) { ((ScriptBase)self)->error_code = error_code; }
+
+static const ScriptSpyMethodStruct kTheMethod = {
+    .New = New,
+    .Delete = Delete,
+    .DeleteCalled = DeleteCalled,
+    .RunCalled = RunCalled,
+    .GetEngine = GetEngine,
+    .GetNotificationCommand = GetNotificationCommand,
+    .SetErrorCode = SetErrorCode,
+};
+
+const ScriptSpyMethod scriptSpy = &kTheMethod;
---
+++ src/script/script_spy_test.cpp
@@ -0,0 +1,60 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "counting_command_stub.h"
+#include "default_active_object_engine.h"
+#include "script/script_spy.h"
+}
+
+class ScriptTest : public ::testing::Test {
+ protected:
+  ActiveObjectEngine e;
+  Command c;
+  Command script;
+
+  virtual void SetUp() {
+    e = defaultActiveObjectEngine->New();
+    c = countingCommandStub->New();
+    script = scriptSpy->New("test");
+    scriptBase->SetEngine(script, e);
+    scriptBase->SetNotificationComand(script, c);
+  }
+
+  virtual void TearDown() {
+    scriptSpy->Delete(&script);
+    c->Delete(&c);
+    e->Delete(&e);
+  }
+};
+
+TEST_F(ScriptTest, Delete) {
+  scriptBase->Delete(&script);
+
+  EXPECT_TRUE(scriptSpy->DeleteCalled(script));
+}
+
+TEST_F(ScriptTest, Run) {
+  scriptBase->Run(script);
+
+  EXPECT_TRUE(scriptSpy->RunCalled(script));
+}
+
+TEST_F(ScriptTest, SetEngine) { EXPECT_EQ(e, scriptSpy->GetEngine(script)); }
+
+TEST_F(ScriptTest, SetNotificationCommand) { EXPECT_EQ(c, scriptSpy->GetNotificationCommand(script)); }
+
+TEST_F(ScriptTest, GetName) { EXPECT_STREQ("test", scriptBase->GetName(script)); }
+
+TEST_F(ScriptTest, GetErrorCode) {
+  scriptSpy->SetErrorCode(script, 1234);
+
+  EXPECT_EQ(1234, scriptBase->GetErrorCode(script));
+}
+
+TEST_F(ScriptTest, HasError) {
+  scriptSpy->SetErrorCode(script, 1234);
+
+  EXPECT_TRUE(scriptBase->HasError(script));
+}
