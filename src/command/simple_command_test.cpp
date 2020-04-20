fa80a0b54f8124e575541aa340141d5da531febd
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-20 15:16:10 +0900

    Add SimpleCommand class

---
+++ src/command/simple_command_test.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "simple_command.h"
+}
+
+namespace {
+bool was_ran;
+
+void DoSpy(void) { was_ran = true; }
+}
+
+class SimpleCommandTest : public ::testing::Test {
+ protected:
+  Command c;
+
+  virtual void SetUp() {
+    was_ran = false;
+    c = simpleCommand->New(DoSpy);
+  }
+
+  virtual void TearDown() { c->Delete(&c); }
+};
+
+TEST_F(SimpleCommandTest, Do) {
+  c->Do(c);
+
+  EXPECT_TRUE(was_ran);
+}
---
+++ v1/simple_command.h
@@ -0,0 +1,16 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_SIMPLE_COMMAND_H_
+#define V1_SIMPLE_COMMAND_H_
+
+#include "command.h"
+
+typedef void (*DoCommandDelegate)(void);
+typedef struct {
+  Command (*New)(DoCommandDelegate delegate);  //
+} SimpleCommandMethodStruct;
+typedef const SimpleCommandMethodStruct* SimpleCommandMethod;
+
+extern const SimpleCommandMethod simpleCommand;
+
+#endif  // V1_SIMPLE_COMMAND_H_
