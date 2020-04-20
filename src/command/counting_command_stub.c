75411ad03c60b0854054b20f2bbefb2f57311c19
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-20 10:25:20 +0900

    Add CountingCommandStub class

---
+++ src/command/counting_command_stub.c
@@ -0,0 +1,37 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "counting_command_stub.h"
+
+#include "bleu/v1/heap.h"
+
+typedef struct {
+  CommandInterfaceStruct impl;
+  int delete_count;
+  int do_count;
+} CountingCommandStubStruct, *CountingCommandStub;
+
+static void DeleteSpy(Command *self) { ++((CountingCommandStub)*self)->delete_count; }
+
+static void DoSpy(Command self) { ++((CountingCommandStub)self)->do_count; }
+
+static Command New(void) {
+  CountingCommandStub self = (CountingCommandStub)heap->New(sizeof(CountingCommandStubStruct));
+  self->impl.Delete = DeleteSpy;
+  self->impl.Do = DoSpy;
+  return (Command)self;
+}
+
+static void Delete(Command *self) { heap->Delete((void **)self); }
+
+static int DeleteCount(Command self) { return ((CountingCommandStub)self)->delete_count; }
+
+static int DoCount(Command self) { return ((CountingCommandStub)self)->do_count; }
+
+static const CountingCommandStubMethodStruct kTheMethod = {
+    .New = New,
+    .Delete = Delete,
+    .DeleteCount = DeleteCount,
+    .DoCount = DoCount,
+};
+
+const CountingCommandStubMethod countingCommandStub = &kTheMethod;
---
+++ src/command/counting_command_stub_test.cpp
@@ -0,0 +1,48 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "counting_command_stub.h"
+}
+
+class CountingCommandStubTest : public ::testing::Test {
+ protected:
+  Command c;
+
+  virtual void SetUp() { c = countingCommandStub->New(); }
+
+  virtual void TearDown() { c->Delete(&c); }
+};
+
+TEST_F(CountingCommandStubTest, ConditionAfterCreation) {
+  EXPECT_EQ(0, countingCommandStub->DeleteCount(c));
+  EXPECT_EQ(0, countingCommandStub->DoCount(c));
+}
+
+TEST_F(CountingCommandStubTest, DeleteCount) {
+  c->Delete(&c);
+  c->Delete(&c);
+  c->Delete(&c);
+
+  EXPECT_EQ(3, countingCommandStub->DeleteCount(c));
+  EXPECT_EQ(0, countingCommandStub->DoCount(c));
+}
+
+TEST_F(CountingCommandStubTest, DoCount) {
+  c->Do(c);
+  c->Do(c);
+  c->Do(c);
+
+  EXPECT_EQ(0, countingCommandStub->DeleteCount(c));
+  EXPECT_EQ(3, countingCommandStub->DoCount(c));
+}
+
+TEST_F(CountingCommandStubTest, Count) {
+  c->Do(c);
+  c->Do(c);
+  c->Delete(&c);
+
+  EXPECT_EQ(1, countingCommandStub->DeleteCount(c));
+  EXPECT_EQ(2, countingCommandStub->DoCount(c));
+}
