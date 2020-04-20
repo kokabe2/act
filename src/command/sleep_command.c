76dd2ea97ac7ee64862147738a93f3758078f8c8
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-20 11:06:46 +0900

    Add SleepCommand class

---
+++ src/command/sleep_command.c
@@ -0,0 +1,54 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "sleep_command.h"
+
+#include <stdbool.h>
+
+#include "bleu/v1/heap.h"
+#include "malkt/v1/time_unit.h"
+
+typedef struct {
+  CommandInterfaceStruct impl;
+  int sleep_time;
+  ActiveObjectEngine engine;
+  Command wakeup_command;
+  bool started;
+  int64_t start_time;
+} SleepCommandStruct, *SleepCommand;
+
+static void Delete(Command *self) { heap->Delete((void **)self); }
+
+inline static bool ShouldWakeUp(SleepCommand self, int64_t current_time) {
+  return current_time >= self->start_time + self->sleep_time;
+}
+
+static void Do(Command base) {
+  int64_t current_time = timeUnit->Now(timeUnit->Millisecond);
+  SleepCommand self = (SleepCommand)base;
+  ActiveObjectEngine e = self->engine;
+  if (!self->started) {
+    self->started = true;
+    self->start_time = current_time;
+    e->AddCommand(e, base);
+  } else if (ShouldWakeUp(self, current_time)) {
+    e->AddCommand(e, self->wakeup_command);
+  } else {
+    e->AddCommand(e, base);
+  }
+}
+
+static Command New(int milliseconds, ActiveObjectEngine engine, Command wakeup_command) {
+  SleepCommand self = heap->New(sizeof(SleepCommandStruct));
+  self->impl.Delete = Delete;
+  self->impl.Do = Do;
+  self->sleep_time = milliseconds;
+  self->engine = engine;
+  self->wakeup_command = wakeup_command;
+  return (Command)self;
+}
+
+static const SleepCommandMethodStruct kTheMethod = {
+    .New = New,
+};
+
+const SleepCommandMethod sleepCommand = &kTheMethod;
---
+++ src/command/sleep_command_test.cpp
@@ -0,0 +1,41 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "counting_command_stub.h"
+#include "default_active_object_engine.h"
+#include "sleep_command.h"
+#include "util/time_unit_stub.h"
+}
+
+class SleepCommandTest : public ::testing::Test {
+ protected:
+  ActiveObjectEngine e;
+  Command wakeup;
+  Command c;
+
+  virtual void SetUp() {
+    timeUnitStub->Reset(0, 20);
+    wakeup = countingCommandStub->New();
+    e = defaultActiveObjectEngine->New();
+    c = sleepCommand->New(100, e, wakeup);
+  }
+
+  virtual void TearDown() {
+    c->Delete(&c);
+    e->Delete(&e);
+    wakeup->Delete(&wakeup);
+  }
+};
+
+TEST_F(SleepCommandTest, Run) {
+  e->AddCommand(e, c);
+
+  e->Run(e);
+
+  EXPECT_EQ(1, countingCommandStub->DoCount(wakeup));
+  int64_t t = timeUnit->Now(timeUnit->Millisecond);
+  EXPECT_GE(t, 100);
+  EXPECT_LE(t, 120);
+}
