cd53432e40eca739237fe0149970c90bca466342
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-20 10:14:40 +0900

    Add NullCommand class

---
+++ src/command/null_command.c
@@ -0,0 +1,31 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "null_command.h"
+
+#include <stddef.h>
+
+#include "bleu/v1/heap.h"
+
+static Command INSTANCE = NULL;
+
+static void Delete(Command *self) {}
+
+static void Do(Command self) {}
+
+static Command New(void) {
+  Command self = heap->New(sizeof(CommandInterfaceStruct));
+  self->Delete = Delete;
+  self->Do = Do;
+  return self;
+}
+
+static Command GetInstance(void) {
+  if (INSTANCE == NULL) INSTANCE = New();
+  return INSTANCE;
+}
+
+static const NullCommandMethodStruct kTheMethod = {
+    .GetInstance = GetInstance,
+};
+
+const NullCommandMethod nullCommand = &kTheMethod;
